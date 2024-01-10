d075ef4e1b48a8a86581ac93ce75cac4fbd3220a
cicospui <1249828424@qq.com>
2024-01-10 19:29:00 +0800

    1
    菜品分页查询

--- code/sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
+++ code/sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 /**
  * @Description:
@@ -10,4 +12,6 @@ import com.sky.dto.DishDTO;
 public interface DishService {
     //新增菜品和对应口味
     public void saveWithFlavor(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
