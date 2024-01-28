7f413f0ff9fcedb481ba606ecb88c4ed10efeb18
cicospui <1249828424@qq.com>
2024-01-28 18:22:05 +0800

    1
    完结

--- code/sky-take-out/sky-server/src/main/java/com/sky/service/ReportService.java
+++ code/sky-take-out/sky-server/src/main/java/com/sky/service/ReportService.java
@@ -5,6 +5,7 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -25,4 +26,7 @@ public interface ReportService {
 
     //统计时间区间内的销量排名前十
     SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+
+    //导出运营数据报表
+    void exportBusinessData(HttpServletResponse response);
 }
